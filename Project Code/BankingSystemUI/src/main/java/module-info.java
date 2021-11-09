e2e41a18897391bc79d526709bfa527857642789
GOLDEN <zsolnai@student.chalmers.se>
2021-11-09 19:45:07 +0100

    hello

--- Project Code/BankingSystemUI/src/main/java/module-info.java
+++ Project Code/BankingSystemUI/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.bankingsystemui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens com.example.bankingsystemui to javafx.fxml;
