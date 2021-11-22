d10420585b658a1e064d867b5975c9d63b2c5a1c
aieh <guseisai2student,gu.se>
2021-11-22 14:00:07 +0100

    Methods for creating accounts

--- CrushersCustomerApp/src/main/java/crushers/model/Customer.java
+++ CrushersCustomerApp/src/main/java/crushers/model/Customer.java
@@ -1,18 +1,28 @@
 package crushers.model;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Customer extends User{
-
+    private final double interestRate = 0.02; // for now
     private ArrayList<PaymentAccount> accountList;
     private ArrayList<Contact> contactList;
 
+
     public Customer(String firstName, String lastName, String address, String email, String password,
-    ArrayList<String> securityQuestions, int ID){
+    ArrayList<String> securityQuestions, int ID) {
         super(firstName, lastName, address, email, password, securityQuestions, ID);
         this.accountList = new ArrayList<PaymentAccount>();
         this.contactList = new ArrayList<Contact>();
-
     }
-    
+    public void createPaymentAccount() {
+        List<Transaction> transactionList = new ArrayList<>();
+        PaymentAccount paymentAccount = new PaymentAccount(0,transactionList);
+        accountList.add(paymentAccount);
+    }
+    public void createSavingsAccount() {
+        List<Transaction> transactionList = new ArrayList<>();
+        PaymentAccount savingsAccount = new SavingsAccount(0,transactionList,interestRate);
+        accountList.add(savingsAccount);
+    }
 }
