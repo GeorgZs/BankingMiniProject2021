0a901af1bf48a0f6fc9c14d0dbc7964e4bf752de
plepisnew <plepis.jaunais@gmail.com>
2021-11-22 11:42:29 +0100

    Fixed argument mismatch and missing methods

--- CrushersCustomerApp/src/main/java/crushers/model/User.java
+++ CrushersCustomerApp/src/main/java/crushers/model/User.java
@@ -6,23 +6,47 @@ public class User {
     private String firstName;
     private String lastName;
     private String address;
-    private String emailAddress;
+    private String email;
     private String password;
     private List<String> securityQuestions;
     // private List<standardAccount> accountList;
     private int id;
     // private int timeStamp;
 
-    public User(String firstName, String lastName, String address, String emailAddress, String password, List<String> securityQuestions, int id) {
+    public User(String firstName, String lastName, String address, String email, String password, List<String> securityQuestions, int id) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
-        this.emailAddress = emailAddress;
+        this.email = email;
         this.password = password;
         this.securityQuestions = securityQuestions;
         this.id = id;
     }
 
+    public String getFirstName(){
+        return this.firstName;
+    }
+    public String getLastName(){
+        return this.lastName;
+    }
+    public String getAddress(){
+        return this.address;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public List<String> getSecurityQuestions(){
+        return this.securityQuestions;
+    }
+    public int getId(){
+        return this.id;
+    }
 
+    public String toString(){
+        return this.firstName + " " + this.lastName + " lives at " + this.address + "\n" + this.email + " " + this.password;
+    }
 
 }
