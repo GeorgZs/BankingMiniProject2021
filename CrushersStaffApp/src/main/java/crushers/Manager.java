86da2724591f7c58f70654a1d5a17546e34b6ad7
bardiaf <bardiaf@student.chalmers.se>
2021-11-23 21:49:37 +0100

    Manager creates account for clerk

---
+++ CrushersStaffApp/src/main/java/crushers/Manager.java
@@ -0,0 +1,40 @@
+package crushers;
+
+import java.util.List;
+
+public class Manager extends Clerk {
+
+    private Bank bank;
+
+    public Manager() {
+        //empty for Jackson
+    }
+
+    public Manager(String firstName, String lastName, String address, String emailAddress, String password, List<String> securityQuestions, Bank employmentBank) {
+        super(firstName, lastName, address, emailAddress, password, securityQuestions, employmentBank);
+    }
+
+    public Clerk createClerkAccount(String firstName, String lastName, String address, String emailAddress, String password, List<String> securityQuestions, Bank employmentBank) {
+        Clerk clerk = new Clerk(firstName, lastName, address, emailAddress, password, securityQuestions, employmentBank);
+        return clerk;
+    }
+
+    public void setBankName(String name) {
+        bank = getEmploymentBank();
+        bank.setName(name);
+    }
+
+    public void setBankLogo(String logo) {
+        bank = getEmploymentBank();
+        bank.setLogo(logo);
+    }
+
+    public void setBankDetails(String details) {
+        bank = getEmploymentBank();
+        bank.setDetails(details);
+    }
+
+    public Bank getEmploymentBank() {
+        return this.bank;
+    }
+}
