efc709adf383eff7e663484432796ace0ee5fb6c
JohnWebb00 <90259425+JohnWebb00@users.noreply.github.com>
2021-11-24 20:18:04 +0100

    Additions to AccountCreationController

--- CrushersCustomerApp/src/main/java/crushers/controllers/AccountCreationController.java
+++ CrushersCustomerApp/src/main/java/crushers/controllers/AccountCreationController.java
@@ -1,4 +1,30 @@
 package crushers.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AccountCreationController {
+
+    @FXML
+    private ChoiceBox bankSelection;
+
+    @FXML
+    private RadioButton paymentOption;
+
+    @FXML
+    private RadioButton savingsOption;
+
+    @FXML
+    private TextArea accountDescription;
+
+    @FXML
+    private TextField savingsGoalField;
+
+
+
+
+
 }
