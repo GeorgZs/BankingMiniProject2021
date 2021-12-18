27bc3ea85d6c3b63d6646af5ddca46a982ddfec9
Ivan <gusvidiv@student.gu.se>
2021-12-18 03:17:17 +0100

    Connected the transactions to the server, found a bug.

--- CrushersStaffApp/src/main/java/crushers/controllers/DepositController.java
+++ CrushersStaffApp/src/main/java/crushers/controllers/DepositController.java
@@ -1,13 +1,25 @@
 package crushers.controllers;
 
+import crushers.WindowManager;
+import crushers.api.Http;
+import crushers.api.HttpError;
+import crushers.api.ServerFacade;
+import crushers.datamodels.Bank;
+import crushers.datamodels.BankAccount;
+import crushers.datamodels.Transaction;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.awt.event.ActionEvent;
+import java.util.List;
 
 public class DepositController {
     @FXML
     private TextField amount;
     @FXML
-    private ChoiceBox<String> accountsTo;
+    private ChoiceBox<BankAccount> accountsTo;
     @FXML
     private TextArea description;
     @FXML
@@ -17,9 +29,59 @@ public class DepositController {
     @FXML
     private Button cancelButton;
 
-    //@FXML
-    //private void onPressedDeposit(ActionEvent actionEvent) throws Exception {
-      //  if()
+    @FXML
+    void initialize() {
+        List<BankAccount> accounts = null;
+        try {
+            accounts = ServerFacade.instance.listAllBankAccountsAtThisBank();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        accountsTo.setItems(FXCollections.observableArrayList(accounts));
+        accountsTo.setStyle("-fx-font-family: SansSerif");
+    }
+
+
+    @FXML
+    private void onPressedDeposit(javafx.event.ActionEvent actionEvent) throws Exception {
+        /*if(Integer.parseInt(amount.getText()) < 0) {
+          amount.setStyle("-fx-border-color: red ; -fx-border-width: 1px");
+          throw new Exception("Amount cannot be negative");
+      } else {
+            amount.setStyle("-fx-border-color: transparent");
+      }
+
+         */
+        Transaction deposit = new Transaction();
+        accountsTo.getValue().setType("payment");
+        deposit.setTo(accountsTo.getValue());
+        deposit.setAmount(Double.parseDouble(amount.getText()));
+        deposit.setDescription(description.getText());
+        //deposit.setDate(dateOnDeposit.get);
+
+        try {
+            ServerFacade.instance.deposit(deposit);
+            System.out.println("Deposited: " + amount.getText());
+            WindowManager.closeModal();
+        } catch (Exception e) {
+            if (e instanceof HttpError) showAlert(((HttpError)e).getError());
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onPressedCancel(javafx.event.ActionEvent actionEvent) throws Exception {
+        WindowManager.closeModal();
+    }
+
+    private void showAlert(String message){
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle("Warning");
+        a.setHeaderText(message);
+        a.getDialogPane().setStyle("-fx-font-family: SansSerif");
+        a.show();
+    }
+
 
 
 }
--- CrushersStaffApp/src/main/java/crushers/controllers/WithdrawController.java
+++ CrushersStaffApp/src/main/java/crushers/controllers/WithdrawController.java
@@ -1,17 +1,23 @@
 package crushers.controllers;
 
+import crushers.WindowManager;
+import crushers.api.HttpError;
+import crushers.api.ServerFacade;
+import crushers.datamodels.BankAccount;
+import crushers.datamodels.Transaction;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class WithdrawController {
     @FXML
     private TextField amount;
     @FXML
-    private ChoiceBox<String> accountFrom;
+    private ChoiceBox<BankAccount> accountFrom;
     @FXML
     private TextArea description;
     @FXML
@@ -21,25 +27,50 @@ public class WithdrawController {
     @FXML
     private Button cancelButton;
 
-    //Testing choicebox
-    private String[] accounts = {
-            "Account 1",
-            "Account 2",
-            "Account 3",
-            "Account 4"
-    };
 
     @FXML
     void initialize(){
-        accountFrom.setItems(FXCollections.observableArrayList(new ArrayList<String>(Arrays.asList(accounts))));
+        List<BankAccount> accounts = null;
+        try {
+            accounts = ServerFacade.instance.listAllBankAccountsAtThisBank();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        accountFrom.setItems(FXCollections.observableArrayList(accounts));
         accountFrom.setStyle("-fx-font-family: SansSerif");
     }
 
     @FXML
-    public void onPressedCancel(javafx.event.ActionEvent actionEvent) throws Exception {}
+    public void onPressedCancel(javafx.event.ActionEvent actionEvent) throws Exception {
+        WindowManager.closeModal();
+
+    }
 
     @FXML
     public void onPressedWithdraw(javafx.event.ActionEvent actionEvent) throws Exception {
+        Transaction withdraw = new Transaction();
+        accountFrom.getValue().setType("payment");
+        withdraw.setTo(accountFrom.getValue());
+        withdraw.setAmount(Double.parseDouble(amount.getText())*-1);
+        withdraw.setDescription(description.getText());
+        //deposit.setDate(dateOnDeposit.get);
+
+        try {
+            ServerFacade.instance.withdraw(withdraw);
+            System.out.println("Withdrew: " + amount.getText());
+            WindowManager.closeModal();
+        } catch (Exception e) {
+            if (e instanceof HttpError) showAlert(((HttpError)e).getError());
+            e.printStackTrace();
+        }
+
     }
 
+    private void showAlert(String message){
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        a.setTitle("Warning");
+        a.setHeaderText(message);
+        a.getDialogPane().setStyle("-fx-font-family: SansSerif");
+        a.show();
+    }
 }
